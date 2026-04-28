7c4c678f0eaf028ab09a45a3ad16dc13ee6728dd
Victor Hugo Pedro <victorh.pedr@gmail.com>
2026-04-27 22:50:28 -0300

    fix: ajusta arquivo

--- app/Providers/TelescopeServiceProvider.php
+++ app/Providers/TelescopeServiceProvider.php
@@ -1,12 +1,18 @@
+<?php
+
+namespace App\Providers;
+
 use App\Models\User;
 use Illuminate\Support\Facades\Gate;
+use Laravel\Telescope\Telescope;
+use Laravel\Telescope\TelescopeApplicationServiceProvider;
 
 protected function gate(): void
 {
     Gate::define('viewTelescope', function (User $user) {
         return in_array($user->email, [
-            'victor.hugo@cartaoamigosaude.com.br',
-            'roberta.cavalheiro@cartaoamigosaude.com.br',
+            'victor.hugo@crm.cartaoamigosaude.com.br',
+            'roberta.cavalheiro@cartaoamigosaude.com.br'
         ]);
     });
 }
