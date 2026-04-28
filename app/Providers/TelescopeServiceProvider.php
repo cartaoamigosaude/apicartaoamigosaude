cf8013959d3d98c6eef9646e275dfd2aa4042325
Victor Hugo Pedro <victorh.pedr@gmail.com>
2026-04-27 22:45:16 -0300

    feat: adiciona usuario telescope

--- app/Providers/TelescopeServiceProvider.php
+++ app/Providers/TelescopeServiceProvider.php
@@ -5,7 +5,8 @@ protected function gate(): void
 {
     Gate::define('viewTelescope', function (User $user) {
         return in_array($user->email, [
-            'victor.hugo@crm.cartaoamigosaude.com.br',
+            'victor.hugo@cartaoamigosaude.com.br',
+            'roberta.cavalheiro@cartaoamigosaude.com.br',
         ]);
     });
 }
