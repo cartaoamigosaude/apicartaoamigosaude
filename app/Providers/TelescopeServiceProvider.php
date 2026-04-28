6ae3290f57835f4a4736bccd29cf16acdb9bd48e
Victor Hugo Pedro <victorh.pedr@gmail.com>
2026-04-27 22:53:48 -0300

    fix: ajusta arquivo

--- app/Providers/TelescopeServiceProvider.php
+++ app/Providers/TelescopeServiceProvider.php
@@ -4,15 +4,34 @@ namespace App\Providers;
 
 use App\Models\User;
 use Illuminate\Support\Facades\Gate;
+use Laravel\Telescope\IncomingEntry;
 use Laravel\Telescope\Telescope;
 use Laravel\Telescope\TelescopeApplicationServiceProvider;
 
-protected function gate(): void
+class TelescopeServiceProvider extends TelescopeApplicationServiceProvider
 {
-    Gate::define('viewTelescope', function (User $user) {
-        return in_array($user->email, [
-            'victor.hugo@crm.cartaoamigosaude.com.br',
-            'roberta.cavalheiro@cartaoamigosaude.com.br'
-        ]);
-    });
+    public function register(): void
+    {
+        $this->hideSensitiveRequestDetails();
+
+        $isLocal = $this->app->environment('local');
+
+        Telescope::filter(function (IncomingEntry $entry) use ($isLocal) {
+            return $isLocal
+                || $entry->isReportableException()
+                || $entry->isFailedJob()
+                || $entry->isScheduledTask()
+                || $entry->isSlowQuery();
+        });
+    }
+
+    protected function gate(): void
+    {
+        Gate::define('viewTelescope', function (User $user) {
+            return in_array($user->email, [
+                'victor.hugo@cartaoamigosaude.com.br',
+                'roberta.cavalheiro@cartaoamigosaude.com.br',
+            ]);
+        });
+    }
 }
